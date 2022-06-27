d9c829fd609665fd10d587b4fd95756dd47b92ce
 Dmytro Puchkov <dmytropuchkov@designer.local>
2022-06-27 21:16:35 +0300

    work with hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('2322Test');
+const menuBtn = document.querySelector('.menu__btn')
+const menuList = document.querySelector('.menu__list')
+const body = document.querySelector('body')
+
+menuBtn.addEventListener('click', (e) => {
+  menuBtn.classList.toggle('active')
+  body.classList.toggle('active')
+  if (menuList.classList.contains('active')) {
+    setTimeout(() => menuList.classList.toggle('active'), 500)
+  } else {
+    menuList.classList.toggle('active')
+  }
+})
