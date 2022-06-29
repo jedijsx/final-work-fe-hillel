99f10a256b607e4162f63575a330621979bff53b
 Dmytro Puchkov <dmytropuchkov@designer.local>
2022-06-29 20:39:49 +0300

    work with slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,32 @@ menuBtn.addEventListener('click', (e) => {
     menuList.classList.toggle('active')
   }
 })
+
+// get DOM elements
+const headersList = document.querySelectorAll('.slider__header')
+const notesList = document.querySelectorAll('.slider__note')
+const indicatorsList = document.querySelectorAll('.slider__indicator')
+const descriptionsList = document.querySelectorAll('.slider__description')
+
+let index = 0;
+let interval = 3500;
+let heightsArr = [];
+let heightMax = null;
+
+setInterval(() => {
+  // remove the active classes for the first elements
+  headersList[index].classList.toggle('active')
+  notesList[index].classList.toggle('active')
+  indicatorsList[index].classList.toggle('active')
+  // increase the index until the number of elements is exceeded
+  index = (index + 1) % headersList.length;
+  // set active classes to the following elements
+  headersList[index].classList.toggle('active')
+  notesList[index].classList.toggle('active')
+  indicatorsList[index].classList.toggle('active')
+}, interval)
+
+// calculating and changing the height of the description block for the maximum text
+descriptionsList.forEach(el => heightsArr.push(el.clientHeight))
+heightMax = Math.max(...heightsArr)
+descriptionsList.forEach(el => el.style.height = `${heightMax}px`)
